91873508e431f50fc138e8fdfd83b66b3fbc8570
Jamie Stevens <ste616@gmail.com>
2019-08-20 23:02:55 +1000

    The PS file now gets generated by the Perl script!

--- json2scd.pl
+++ json2scd.pl
@@ -5,6 +5,7 @@
 use Data::Dumper;
 use JSON;
 use DateTime;
+use POSIX;
 use strict;
 
 my $json = JSON->new->allow_nonref;
@@ -118,6 +119,131 @@ if ($infile =~ /\.json$/) {
     
     print O "</Program>\n";
     close(O);
+
+    # Make the Postscript output.
+    my $t = &stringToDatetime($prog->{'term'}->{'start'});
+    my $te = &stringToDatetime($prog->{'term'}->{'end'});
+    my $dur = $te->epoch() - $t->epoch();
+    my $days = 14;
+    my $totalDays = $dur / 86400;
+    my $numberOfPages = $totalDays / $days + 1;
+    
+    my $outps = $infile;
+    $outps =~ s/\.json$/.ps/;
+    open(P, ">".$outps) || die "cannot open $outps for writing";
+    open(A, "schedulea.ps");
+    while(<A>) {
+	print P $_;
+    }
+    close(A);
+    print P "/tel (".uc($prog->{'observatory'}->{'observatory'}).
+	") def\n";
+    open(U, "prepage.ps");
+    my $preUrl = do { local $/; <U> };
+    close(U);
+    open(U, "postpage.ps");
+    my $postUrl = do { local $/; <U> };
+    close(U);
+
+    for (my $i = 0; $i < $numberOfPages; $i++) {
+	print P "%%Page: ". ($i + 1)." ".($i + 1)."\n";
+	print P $preUrl."\n";
+	print P &printps($prog, $i);
+	print P $postUrl."\n";
+    }
+    
+    print P "%%EOF\n";
+    
+    close(P);
+}
+
+sub printps($$) {
+    my $prog = shift;
+    my $pageNumber = shift;
+
+    my $days = 14;
+    my $pageLength = $days * 86400;
+
+    my $time1 = &stringToDatetime($prog->{'term'}->{'start'});
+    my $d = $time1->day_of_week();
+    $time1->subtract( days => (($d + 6) % 7) );
+
+    my $rstring = "";
+    # This is the time at the top of the page.
+    $time1->add( days => ($pageNumber * $days) );
+    # This is the time at the bottom of the page.
+    my $time2 = $time1->clone();
+    $time2->add( days => $days );
+
+    my $version = $prog->{'term'}->{'version'};
+    my $today = DateTime->now;
+    $rstring .= "(".($pageNumber + 1).") (".$version.") (".
+	$today->dmy("/").") page\n";
+    $rstring .= "/dd ".$time1->day." def\n";
+    $rstring .= "/mon ".$time1->month." def\n";
+    $rstring .= "/year ".$time1->year." def\n";
+
+    my $day1 = 0;
+    my $day2 = 0;
+    my $config = &getConfig($prog, $time1);
+    
+    # Cycle through the slots.
+    for (my $i = 0; $i <= $#{$prog->{'project'}}; $i++) {
+	my $proj = $prog->{'project'}->[$i];
+	for (my $j = 0; $j <= $#{$proj->{'slot'}}; $j++) {
+	    my $slot = $proj->{'slot'}->[$j];
+	    if ($slot->{'scheduled'} == 0) {
+		next;
+	    }
+	    my $slotStart = DateTime->from_epoch(
+		epoch => $slot->{'scheduled_start'}
+		);
+	    $slotStart->add( hours => 10 );
+	    my $slotEnd = $slotStart->clone();
+	    $slotEnd->add( hours => $slot->{'scheduled_duration'} );
+	    if ((($slotStart > $time1) && ($slotStart < $time2))  ||
+		(($slotEnd > $time1) && ($slotEnd < $time2))) {
+		my $t1 = $slotStart->clone();
+		my $t0 = $t1->clone();
+		$t0->set( hour => 0, minute => 0, second => 0 );
+		if ($t1 < $time1) {
+		    $t1 = $time1->clone();
+		    $t0 = $time1->clone();
+		}
+		my $t2 = $t0->clone();
+		$t2->add( days => 1 );
+		if ($proj->{'ident'} eq "CONFIG") {
+		    my $day2 = floor(($t1->epoch - $time1->epoch) / 86400) + 1;
+		    if ($day1 != $day2) {
+			$rstring .= $day1." ".$day2." (".$config.") config\n";
+			$rstring .= &getConfigPS($config);
+		    }
+		    $config = uc($slot->{'array'});
+		    $day1 = $day2;
+		}
+		while (($t1 < $slotEnd) && ($t1 < $time1)) {
+		    $t1 = $t2->clone();
+		    $t2->add( days => 1 );
+		}
+		while (($t2 < $slotEnd) && ($t2 <= $time2)) {
+		    $rstring .= &ps_sch_box($proj, $slot, $t1, $t2);
+		    $t1 = $t2->clone();
+		    $t2->add( days => 1);
+		}
+		$t2 = $slotEnd->clone();
+		if ($t2 <= $time2) {
+		    $rstring .= &ps_sch_box($proj, $slot, $t1, $t2);
+		}
+	    }
+	}
+    }
+    $day2 = 14;
+    if ($day1 != $day2) {
+	$rstring .= $day1." ".$day2." (".$config.") config\n";
+	$rstring .= &getConfigPS($config);
+    }
+    
+    return $rstring;
 }
 
 sub commentWriter($) {
@@ -184,4 +310,94 @@ sub epoch2timeString($$) {
 	$dt->set_time_zone("Australia/Sydney");
     }
     return $dt->strftime($fmt);
+}
+
+sub stringToDatetime($) {
+    my $dstring = shift;
+
+    my @dels = split(/\-/, $dstring);
+    my $dt = DateTime->new(
+	year => $dels[0], month => $dels[1], day => $dels[2],
+	hour => 0, minute => 0, second => 0 );
+    return $dt;
+}
+
+sub getConfig($$) {
+    my $prog = shift;
+    my $dt = shift;
+
+    # Find the config at the time.
+    my $cfg = "";
+    my $cfgdiff = 1e9;
+    for (my $i = 0; $i <= $#{$prog->{'project'}}; $i++) {
+	if ($prog->{'project'}->[$i]->{'ident'} ne "CONFIG") {
+	    next;
+	}
+	
+	for (my $j = 0; $j <= $#{$prog->{'project'}->[$i]->{'slot'}}; $j++) {
+	    my $slot = $prog->{'project'}->[$i]->{'slot'}->[$j];
+	    if ($slot->{'scheduled_start'} == 0) {
+		next;
+	    }
+	    my $d = $dt->epoch - ($slot->{'scheduled_start'} + 36000);
+	    if (($d > 0) && ($d < $cfgdiff)) {
+		$cfgdiff = $d;
+		$cfg = $slot->{'array'};
+	    }
+	}
+    }
+
+    return uc($cfg);
+}
+
+sub getConfigPS($) {
+    my $config = shift;
+    chomp(my $resline = `grep $config Confign.txt`);
+    if ($resline ne "") {
+	my @rels = split(/\t/, $resline);
+	return "(".$rels[0].") (".$rels[1].") (".$rels[2].") array\n";
+    }
+    return "% no such array\n";
+}
+
+sub ps_sch_box($$$) {
+    my $proj = shift;
+    my $slot = shift;
+    my $t1 = shift;
+    my $t2 = shift;
+    
+    my $rstring = "";
+
+    my $dfh1 = 24 * ($t1->mjd() - floor($t1->mjd()));
+    my $dfh2 = 24 * ($t2->mjd() - floor($t1->mjd()));
+    my $ddh = $dfh2 - $dfh1;
+    
+    my $h10 = 0.1 * int(10 * $dfh1 + 0.5);
+    my $d10 = 0.1 * int(10 * $ddh + 0.5);
+    my $numOfChar = 100;
+    if ($d10 > 0.1) {
+	my $line = ( ' ' x $numOfChar );
+	my $tstring = sprintf "%d %d %d dop %.1f", $t1->day(),
+	$t1->month(), $t1->year(), $h10;
+	substr($line, 0, length($tstring)) = $tstring;
+
+	$tstring = sprintf "%.1f", $d10;
+	substr($line, 22, length($tstring)) = $tstring;
+	if ($proj->{'ident'} eq "CONFIG") {
+	    $tstring = sprintf " (%s) cfg_box", $slot->{'source'};
+	    substr($line, 30, length($tstring)) = $tstring;
+	} elsif ($proj->{'ident'} eq "MAINT") {
+	    $tstring = " () mnt_box";
+	    substr($line, 30, length($tstring)) = $tstring;
+	} else {
+	    $tstring = sprintf " (%s) ((%s)) ((%s)) ((%s)) (%s) (%s) sch_box",
+	    $proj->{'ident'}, $proj->{'PI'}, join(" ", @{$slot->{'bands'}}),
+	    $slot->{'bandwidth'}, "", $slot->{'source'};
+	    substr($line, 30, length($tstring)) = $tstring;
+	}
+	$rstring .= $line."\n";
+    }
+    
+    return $rstring."\n";
+    
 }
