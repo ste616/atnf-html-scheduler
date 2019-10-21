9604e4e95fd36fc8662b869f044bc29bf80e4f92
Jamie Stevens <ste616@gmail.com>
2019-10-21 15:46:12 +1100

    Start making the statistics plot page

---
+++ statistics.js
@@ -0,0 +1,29 @@
+/**
+ * Statistics plotter for ATCA and Parkes.
+ * Jamie Stevens 2019
+ */
+
+// Load our data.
+var atcaFile = "atca_statistics.json";
+var parkesFile = "parkes_statistics.json";
+
+const loadFile = function(fname, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', fname, true);
+  xhr.reponseType = "json";
+  xhr.onload = function() {
+    var status = xhr.status;
+    if (status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
+  xhr.send();
+};
+
+const fileLoaded = function(data) {
+  console.log(fileLoaded);
+};
+
+var atcaStats = loadFile(atcaFile, fileLoaded);
